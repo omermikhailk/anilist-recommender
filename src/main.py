1e951e897ee3b56915ff541d3bd7c87e2995d428
Omer Mikhail Khan <106922795+omermikhailk@users.noreply.github.com>
2022-08-27 00:06:14 +0000

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,38 @@
+from user import User
+
+
+query = """
+    query ($page: Int, $perPage: Int, $userName: String, $type: MediaType) {
+        Page (page: $page, perPage: $perPage) {
+            mediaList (userName: $userName, type: $type) {
+                media {
+                    title {
+                        english
+                        romaji
+                    }
+
+                    episodes
+
+                    chapters
+                    volumes
+
+                    isAdult
+
+                    genres
+                    description
+
+                    averageScore
+                }
+            }
+        }
+
+        GenreCollection
+    }
+"""
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
