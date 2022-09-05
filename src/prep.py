dc1a830d50151533d6f79866e3415707ab0e051d
Omer Mikhail Khan <contact@omermikhailk.dev>
2022-09-05 19:58:27 +0000

    Remove `arg.genre` flattening function
    - Redundant since now the genres are already flattened, directly in
      `args.py`

--- src/prep.py
+++ src/prep.py
@@ -39,24 +39,6 @@ def get_lowercase_genres(genres: list[str]) -> list[str]:
     return list(map(str.lower, genres))
 
 
-# Arg cleaning
-
-def get_user_genres(genres: list[list[str]]) -> list[str]:
-    """Takes in a list of genres, from the command line `--genre` argument
-    input, which is a 2D list, and returns the flattened version of it.
-
-    [['slice', 'of', 'life'], ['comedy']] -> ['slice of life', 'comedy']
-
-    Args:
-        genres (list[list[str]]): A 2D list of the genres that the user is
-            interested in.
-
-    Returns:
-        list[str]: The same genres, however in a 1D list.
-    """
-    return [' '.join(genre) for genre in genres]
-
-
 query_url = 'https://graphql.anilist.co'
 anilist_genres = get_anilist_genres()
 
