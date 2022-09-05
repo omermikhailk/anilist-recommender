ee4a0d7c53771df9d8a3efa39714e074f3e24005
Omer Mikhail Khan <contact@omermikhailk.dev>
2022-09-05 19:13:27 +0000

    Replace `partial-match` with `strict-match`
    - Replace `partial-match` with `strict-match` and change matching
      functionality in general
        - Fix `check_args` logic for it as well
    - Fix issue with the arg list for genres being a 2D list insted of 1D

--- src/args.py
+++ src/args.py
@@ -14,11 +14,18 @@ media (anime or manga).
 
 Optional arguments include:
 
-* The preferred number of recommendations you would like to receive.
-* Preferred genres (written like --genre slice of life --genre comedy, etc.)
-    * Whether the user wants a full match or a partial match of genres. The
-      default is a full match. If partial match is selected then only one
-      genre should be given as the input.
+* The preferred number of recommendations the user would like to receive.
+* Preferred genres (written like `--genre slice of life --genre comedy` and so
+  on.
+    * Whether the user wants a strict match or not.
+
+      By default the program does not use strict matching. If a user wants
+      entries which have the `Shounen` and `Drama` genres, then the program
+      looks for entries which satisfy those requirements. However any entries
+      with additional genres do not have any adverse effect.
+
+      When a strict match is preferred though, then the only matching entries
+      are those, whose sole two genres are `Shounen` and `Drama`.
     * The full list of available genres on AniList are:
         - Action           - Adventure        - Comedy
         - Drama            - Ecchi            - Fantasy
@@ -27,9 +34,9 @@ Optional arguments include:
         - Psychological    - Romance          - Sci-Fi
         - Slice of Life    - Sports           - Supernatural
         - Thriller
-* A lower bound on the number of episodes/chapters/volumes
-* An upper bound on the number of episodes/chapters/volumes
-* Whether the user is okay with series marked as 'Adult' on AL
+* A lower bound on the number of episodes/chapters/volumes.
+* An upper bound on the number of episodes/chapters/volumes.
+* Whether the user is okay with series marked as 'Adult' on AL.
 
 Using these options a list of anime/manga are returned, along with some
 information about them.
@@ -38,19 +45,21 @@ information about them.
 help_username = 'An AniList username.'
 help_type = ('The type of media (anime or manga) that the user is interested '
              'in.')
-help_count = ('The number of recommendations you would like to receive '
-              '(default is 5).')
+help_count = ('The number of recommendations the user would like to receive '
+              '(default = 5).')
 help_genre = 'One or more AniList genres.'
-help_partial_match = ('Whether the user wants a partial match of genres '
-                      '(default false).')
+help_strict_match = ('Whether the user wants a strict match of genres or not '
+                     '(default = False).')
 help_lower_bound = 'A lower bound on the number of episodes/chapters/volumes.'
 help_upper_bound = 'An upper bound on the number of episodes/chapters/volumes.'
 help_adult = ('Whether the user is okay with series marked as \'Adult\' '
-              '(default false).')
+              '(default = False).')
 
 
 def add_args() -> argparse.Namespace:
-    """Adds command line arguments that the user can use and returns them.
+    """Adds command line arguments that the user can use and returns them. Also
+    fixes any input problems, such as `args.genre` bieng a 2D list instead of a
+    1D list.
 
     Returns:
         args (argparse.Namespace): The arguments that the user entered
@@ -75,9 +84,9 @@ def add_args() -> argparse.Namespace:
                         help=help_genre,
                         action='append',
                         nargs='+')
-    parser.add_argument('-pm',
-                        '--partial-match',
-                        help=help_partial_match,
+    parser.add_argument('-sm',
+                        '--strict-match',
+                        help=help_strict_match,
                         action="store_true",
                         default=False)
     parser.add_argument('-lb',
@@ -96,6 +105,13 @@ def add_args() -> argparse.Namespace:
 
     args = parser.parse_args()
 
+    # Fixes the 2D list problem for `args.genre` and makes sure everything is
+    # lowercase.
+    # [['Slice', 'of', 'Life'], 'drama'] -> ['slice of life', 'drama']
+    if args.genre is not None:
+        args.genre = list(map(str.lower,
+                              [' '.join(genre) for genre in args.genre]))
+
     return args
 
 
@@ -108,11 +124,12 @@ def check_args(args: argparse.Namespace) -> None:
             and their values.
 
     Raises:
+        ValueError: if `count` <= 0
         ValueError: if `upper-bound` <= 0
         ValueError: if `lower-bound` <= 0
         ValueError: if `upper-bound` < `lower-bound`
         ValueError: if `genre` is not a valid AniList genre
-        ValueError: if `partial-match` is true but there are no genres
+        ValueError: if `strict_match` is true but there are no genres
     """
     # `count`
     if args.count <= 0:
@@ -136,20 +153,15 @@ def check_args(args: argparse.Namespace) -> None:
 
     # `genre`
     if args.genre is not None:
-        user_genres = prep.get_user_genres(args.genre)
-        anilist_genres = prep.get_lowercase_genres(prep.anilist_genres)
+        anilist_genres_lowercase = list(map(str.lower, (prep.anilist_genres)))
 
-        for genre in user_genres:
-            if genre not in anilist_genres:
+        for genre in args.genre:
+            if genre not in anilist_genres_lowercase:
                 raise ValueError(f'\'{genre}\' is not a valid AniList genre')
-
-        # `partial-match`
-        if args.partial_match and len(user_genres) > 1:
-            raise ValueError(('only one genre input is supported with the '
-                              '--partial-match flag'))
+    # `strict-match`
     else:
-        if args.partial_match:
-            raise ValueError('partial-match requires you to add genres')
+        if args.strict_match:
+            raise ValueError('strict-match requires at least one genre')
 
 
 def main():
